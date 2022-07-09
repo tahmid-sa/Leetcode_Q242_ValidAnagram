abc00158d66a47a3b0e86ee3aa1b92cf1a725673
Tahmid Sajin <tahmid.sajin.1999@gmail.com>
2022-07-09 01:48:25 -0400

    Adding files to github

---
+++ Q242_ValidAnagram/Q242_ValidAnagram.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+bool isAnagram(string s, string t) {
+    if (s.size() != t.size()) {
+        return false;
+    }
+
+    unordered_multiset<char> lettersS;
+
+    for (int i = 0; i < s.size(); i++) {
+        lettersS.insert(s[i]);
+    }
+
+    for (int i = 0; i < t.size(); i++) {
+        if (lettersS.find(t[i]) != lettersS.end()) {
+            lettersS.erase(lettersS.find(t[i]));
+        }
+        else {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    cout << isAnagram("anagram", "nagaram");
+
+    return 0;
+}
